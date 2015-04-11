da33e2f5d8795aeaec38f8f0d47ba7dce10a90b0
mguina <mguina@users.noreply.github.com>
2015-04-11 13:08:22 +0100

    Tidy up

--- NumberGame.java
+++ NumberGame.java
@@ -4,16 +4,16 @@ import java.util.Random;
 
 public class NumberGame {
     public static void main(String args[]){
-        Scanner keyboard = new Scanner(System.in); //this is where you get the input system.in is the keyboard.
+        Scanner keyboard = new Scanner(System.in);
                 
-                out.print("Enter the maximum number: "); //user chooses how many possibilities there will be.
+        out.print("Enter the maximum number: ");
                         
-                        int maxNo = keyboard.nextInt(); 
+        int maxNo = keyboard.nextInt(); 
                                 
-        out.print("Enter any number from 1 to 10: ");
+        out.print("Enter any number from 1 to " + maxNo +": ");
 
         int inputNo = keyboard.nextInt();
-        int randomNo = new Random() .nextInt(2) + 1; // the number in the brackets is the number of possibilities from which the random number can be chosen.
+        int randomNo = new Random() .nextInt(maxNo) + 1;
         
         if (inputNo == randomNo) {
             out.println(" ***************");
